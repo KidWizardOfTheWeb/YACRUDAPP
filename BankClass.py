934773417fe9696c56a198c8130131549eea0970
KidWizardOfTheWeb <smasih2020@gmail.com>
2026-07-27 10:49:34 -0400

    Added bank instance to main, global

--- BankClass.py
+++ BankClass.py
@@ -3,7 +3,7 @@ import uuid
 # THIS IS OUR MAIN CLASS
 class Bank:
     # All accounts, key = UUID, value = account obj
-    account_dict = dict
+    account_dict = {}
 
     # Functions to search for all accounts with X property
     # Filters through users that have a balance based on basic conditionals (greater than, less than, etc)
--- Welcome.py
+++ Welcome.py
@@ -1,15 +1,40 @@
+import BankClass
 
+# Bank instance is global
+bank_inst = BankClass.Bank()
 
 OPTION_LIST = ("1. View account"
                "2. Update accounts"
                "3. Add accounts"
                "4. Remove accounts")
 
+# All valid options for our menu, ranges from 1 to end option
+VALID_OPTIONS = [str(x) for x in range(1, 4)]
+
+# FUNC_LIST = {
+#     "1": func
+# }
+
+
+# View accounts from the account_dict
+# If param sent is "ALL", print all accounts.
+def view_account():
+
+    account_query = input("Type the UUID of the account to display, or type ALL to see all accounts:")
+
+    if account_query != "ALL":
+        print(bank_inst.account_dict[account_query])
+        pass
+    pass
 
 # Treat main as menu selector
 if __name__ == "__main__":
     print(OPTION_LIST)
     user_opt = str(input("Select an option with a number:"))
 
+    if user_opt in VALID_OPTIONS:
+        # Select the matching functions
 
+
+        pass
     pass
