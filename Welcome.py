a81f414ca5594024896c26cab1c531a8a8e30b1e
KidWizardOfTheWeb <smasih2020@gmail.com>
2026-07-27 11:01:49 -0400

    FUNC_LIST functional, updated view_account further

--- Welcome.py
+++ Welcome.py
@@ -3,38 +3,50 @@ import BankClass
 # Bank instance is global
 bank_inst = BankClass.Bank()
 
-OPTION_LIST = ("1. View account"
-               "2. Update accounts"
-               "3. Add accounts"
-               "4. Remove accounts")
+# View accounts from the account_dict
+# If param sent is "ALL", print all accounts.
+def view_account():
+    # If no accounts, print that there are none.
+    if bank_inst.account_dict is None:
+        print("No accounts to show!")
+        pass
+
+    account_query = str(input("Type the UUID of the account to display, or type ALL to see all accounts:"))
+
+    if account_query == "ALL":
+        # Print all account values
+        print(bank_inst.account_dict.values())
+    else:
+        # Else, try to retrieve the single instance.
+        # If not found, return string
+        retrieved_account = bank_inst.account_dict.get(account_query, "No account found.")
+        print(retrieved_account)
+    pass
+
+
+OPTION_LIST = ("1. View account\n"
+               "2. Update accounts\n"
+               "3. Add accounts\n"
+               "4. Remove accounts\n")
 
 # All valid options for our menu, ranges from 1 to end option
 VALID_OPTIONS = [str(x) for x in range(1, 4)]
 
-# FUNC_LIST = {
-#     "1": func
-# }
-
-
-# View accounts from the account_dict
-# If param sent is "ALL", print all accounts.
-def view_account():
-
-    account_query = input("Type the UUID of the account to display, or type ALL to see all accounts:")
-
-    if account_query != "ALL":
-        print(bank_inst.account_dict[account_query])
-        pass
-    pass
+FUNC_LIST = {
+    "1": view_account
+}
 
 # Treat main as menu selector
 if __name__ == "__main__":
-    print(OPTION_LIST)
-    user_opt = str(input("Select an option with a number:"))
 
-    if user_opt in VALID_OPTIONS:
-        # Select the matching functions
+    # Loop until end of time.
+    while True:
+        print(OPTION_LIST)
+        user_opt = str(input("Select an option with a number:"))
 
+        if user_opt in VALID_OPTIONS:
+            # Select the matching functions
+            FUNC_LIST[user_opt]()
 
+            pass
         pass
-    pass
